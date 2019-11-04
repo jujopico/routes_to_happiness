ac080afa18c8f8244e4e9a5687ddd4437860e4e6
jujopico <sujunqt@gmail.com>
2019-11-04 13:28:59 -0500

    routes_to_happiness

--- React_Routes/src/index.js
+++ React_Routes/src/index.js
@@ -7,12 +7,29 @@ import Sleepy from './components/Sleepy';
 import Guilty from './components/Guilty';
 
 // your code goes here
+import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom'
+
 class Home extends Component{
   render(){
     return(
       // your code goes here
-    )
-  }
-}
+      <BrowserRouter>
+      <div>
+        <nav>
+          <ul>
+            <li><NavLink exact to="/">Home</NavLink></li>
+            <li><NavLink exact to="/happy">Happy</NavLink></li>
+            <li><NavLink exact to="/guilty">Guilty</NavLink></li>
+            <li><NavLink exact to="/sleepy">Sleepy</NavLink></li>
+          </ul>
+        </nav>
+        <Switch>
+          <Route exact path="/happy" component={Happy} />
+          <Route exact path="/guilty" component={Guilty} />
+          <Route exact path="/sleepy" component={Sleepy} />
+        </Switch>
+      </div>
+      </BrowserRouter>
+    )}}
 
 ReactDOM.render( <Home />, document.getElementById('root') );
